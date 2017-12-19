4ce0677a6e0d4c8d6509eef95bc79c38bf68211c
Roman_Lutsenko <roman_lutsenko@epam.com>
2017-12-19 15:34:48 +0200

    Task 11. Changed model of jsp control. Deleted files of containers.

--- task10_gradle/src/main/java/com/epam/preprod/roman_lutsenko/commands/Command.java
+++
@@ -1,9 +0,0 @@
-package com.epam.preprod.roman_lutsenko.commands;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-public interface Command {
-
-    void execute(HttpServletRequest request, HttpServletResponse responce);
-}
--- task10_gradle/src/main/java/com/epam/preprod/roman_lutsenko/constants/CommandsConstants.java
+++
@@ -1,5 +0,0 @@
-package com.epam.preprod.roman_lutsenko.constants;
-
-public class CommandsConstants {
-
-}
